8654dcd99b2e5fb2d942d074fac1eeeb968c74ac
Sharan Irani <iranisharan@gmail.com>
2026-04-03 15:41:29 +0530

    checkpoint

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,5 +2,6 @@ export { Button, buttonVariants } from "./button";
 export { Badge, badgeVariants } from "./badge";
 export * from "./card";
 export * from "./icon";
+export { DynamicIcon } from "./icon";
 export * from "./separator";
 
